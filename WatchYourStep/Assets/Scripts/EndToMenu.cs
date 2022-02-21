4c09429f8e73b17408897f5014bf172881585168
iArctix <connortodd.ct@gmail.com>
2022-02-21 14:49:38 +0000

    ENd Scene swag now

---
+++ WatchYourStep/Assets/Scripts/EndToMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndToMenu : MonoBehaviour
+{
+   public void endmenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
