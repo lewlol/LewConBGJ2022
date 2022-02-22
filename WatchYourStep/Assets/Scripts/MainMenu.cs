8766b3754ee8cd165c9e03c6d98fa0c3c75e68c0
lewlol <lewdevbusiness@gmail.com>
2022-02-22 13:26:30 +0000

    MainmenuFixes

--- WatchYourStep/Assets/Scripts/MainMenu.cs
+++ WatchYourStep/Assets/Scripts/MainMenu.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
+    public GameObject playButton;
+    public GameObject quitButton;
+    public GameObject easyText;
+    public GameObject hardText;
+
     public void Play()
     {
-        SceneManager.LoadScene(1);
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        easyText.SetActive(true);
+        hardText.SetActive(true);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void EasyMode()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void HardMode()
+    {
+        SceneManager.LoadScene(2);
     }
 
 }
