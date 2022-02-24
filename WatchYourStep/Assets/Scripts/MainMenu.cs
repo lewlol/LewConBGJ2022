642443c27853c4837966bf244af239e04f8d9557
lewlol <lewdevbusiness@gmail.com>
2022-02-24 14:03:25 +0000

    MAINMENUUQUSWAG

--- WatchYourStep/Assets/Scripts/MainMenu.cs
+++ WatchYourStep/Assets/Scripts/MainMenu.cs
@@ -8,32 +8,28 @@ public class MainMenu : MonoBehaviour
 {
 
     public GameObject playButton;
-    public GameObject easyText;
-    public GameObject hardText;
 
     public Animator anim;
 
+    public GameObject text1;
+    public GameObject text2;
+
     public void Play()
     {
-        playButton.SetActive(false);
-        easyText.SetActive(true);
-        hardText.SetActive(true);
-    }
-
-    public void EasyMode()
-    {
-        Debug.Log("Enter In Easy Mode");
-    }
-    public void HardMode()
-    {
-        Debug.Log("Enter In Hard Mode");
         anim.SetBool("ChangingScene", true);
         StartCoroutine(ChangeToLevel());
     }
 
     IEnumerator ChangeToLevel()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
+        text1.SetActive(true);
+        yield return new WaitForSeconds(13f);
+        text2.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        text1.SetActive(false);
+        text2.SetActive(false);
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("Level");
     }
 
