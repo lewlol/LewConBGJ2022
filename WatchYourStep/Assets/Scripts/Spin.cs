4b6076e973fc7b3f8963a44ab01d2d77cacf7dfd
iArctix <connortodd.ct@gmail.com>
2022-02-24 13:45:47 +0000

    sad

---
+++ WatchYourStep/Assets/Scripts/Spin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(0, speed, 0);
+    }
+
+}
